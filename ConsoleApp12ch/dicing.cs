a45a72e63a3312f19a5efa170c8c44c2182b628b
davidsleeer <102831215@student.swin.edu.au>
2019-09-11 17:13:17 +1000

    extend

---
+++ ConsoleApp12ch/dicing.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+public class DiceSelection
+{
+	public int Calculating(int i, int j )
+	{
+        int pc = 0;
+        int dices = i;
+        int faces = j;
+        var rand = new Random();
+        for (int n=0; n<dices; n++)
+        {
+            pc += rand.Next(1, j+1);
+        }
+        return pc;
+
+
+    }
+}
